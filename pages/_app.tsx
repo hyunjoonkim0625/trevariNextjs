2dc7432ec7a82b44f5c7458d3fb691d6aa516fe7
Hyunjoon Kim <hyunjoon@trevari.co.kr>
2019-05-14 17:10:30 +0900

    로컬에서 앱을 실행시켰을 경우 CSS가 첫 렌더에 표시되지 않는 것을 방지하기 위한 코드 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import App, { Container, NextAppContext } from "next/app";
+import Router from "next/router";
 import React from "react";
 
 import "../index.scss";
@@ -8,6 +9,16 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import Layout from "../Layout/Layout";
 library.add(faBars);
+
+Router.events.on("routeChangeComplete", () => {
+  if (process.env.NODE_ENV !== "production") {
+    const els: NodeListOf<HTMLLinkElement> = document.querySelectorAll(
+      'link[href*="/_next/static/css/styles.chunk.css"]'
+    );
+    const timestamp = new Date().valueOf();
+    els[0].href = "/_next/static/css/styles.chunk.css?v=" + timestamp;
+  }
+});
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }: NextAppContext) {
