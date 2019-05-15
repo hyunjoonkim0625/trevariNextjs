459f5f2e53e8a5a4283fc065816a8f20885b42bc
Hyunjoon Kim <hyunjoon@trevari.co.kr>
2019-05-15 17:18:43 +0900

    MeetingsView.tsx에 debounce 함수 추가

--- components/MeetingsView/MeetingsView.tsx
+++ components/MeetingsView/MeetingsView.tsx
@@ -29,13 +29,14 @@ class MeetingsView extends React.Component<
   // 컴포넌트가 렌더되었을 때 보여지는 아이템의 갯수를 제한
   componentDidMount() {
     const { clubList } = this.props;
+    const debouncedScroll = debounced(1000, this.onScroll);
 
     this.setState({
       currentlyShownList: clubList,
       loading: false
     });
 
-    window.addEventListener("scroll", this.onScroll, false);
+    window.addEventListener("scroll", debouncedScroll, false);
   }
 
   componentWillUnmount() {
@@ -170,63 +171,17 @@ class MeetingsView extends React.Component<
 
 export default MeetingsView;
 
-// {
-//   hasData ? (
-//     // 이미지가 로드가 된 후에 콘텐츠를 화면에 렌더하기 위핸 클래스 추가
-//   <div>
-//     {/* FIXED: currentlyShownList가 없는 경우가 없을 것 같아 해당 체킹은 생략해도 괜찮을 것 같아요. */}
-//     {/* 초기 상태값에 []를 할당하기 때문에 */}
-//     {currentlyShownList
-//       .slice(0, items)
-//       // FIXED: 여기도 any 자제! -> 맞는 방법일까?
+// debouncer
 
-//       .map((item, index: number) => {
-//         return (
-//           // eslint-disable-next-line jsx-a11y/anchor-is-valid
-//           <div className="MeetingsView__clubList__content" key={index}>
-//             <div className="MeetingsView__clubList__content__image">
-//               <img
-//                 src={item.imgSrc}
-//                 alt={item.bookTitle}
-//                 onLoad={() =>
-//                   this.setState({
-//                     loading: false
-//                   })
-//                 }
-//               />
-//               {item.clubRep ? (
-//                 <p className="MeetingsView__clubList__content__image__clubRep">
-//                   클럽장 {item.clubRep}님
-//                       </p>
-//               ) : null}
-//               {item.trevariDesigned ? (
-//                 <p className="MeetingsView__clubList__content__image__trevari">
-//                   트레바리가 디자인한 클럽
-//                       </p>
-//               ) : null}
-
-//               <div className="MeetingsView__clubList__content__image__clubInfo">
-//                 <p>{item.clubName}</p>
-//                 <p>{item.clubDescription}</p>
-//               </div>
-//             </div>
-
-//             <p className="MeetingsView__clubList__content__bookTitle">
-//               {item.bookTitle}
-//             </p>
-//             <p className="MeetingsView__clubList__content__location">
-//               {item.location}
-//             </p>
-//             <p className="MeetingsView__clubList__content__time">
-//               {item.time}
-//             </p>
-//           </div>
-//         );
-//       })}
-//   </div>
-// ) : (
-//     <div className="MeetingsView__notFound">
-//       <div>검색 결과가 없습니다.</div>
-//     </div>
-//     )
-// }
+function debounced(delay: number, fn: any) {
+  let timerId: any;
+  return function(...args: any) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      fn(...args);
+      timerId = null;
+    }, delay);
+  };
+}
