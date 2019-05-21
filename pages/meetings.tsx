b2d8a34c2df5408ea16b4a5064ae87a45eff7cb1
Hyunjoon Kim <hyunjoon@trevari.co.kr>
2019-05-21 16:49:25 +0900

    불필요한 코드 수정: MeetingsView의 코드가 이 컴포넌트로 옮겨와도 됨

--- pages/meetings.tsx
+++ pages/meetings.tsx
@@ -1,20 +1,6 @@
 import { NextFunctionComponent } from "next";
 import MeetingsView from "../components/MeetingsView";
-import { IClub } from "../interfaces";
-import { getClubList } from "../utils/mockData";
 
-type MeetingsPageProps = {
-  clubListData: IClub[];
-};
-
-const MeetingsPage: NextFunctionComponent<MeetingsPageProps> = ({
-  clubListData
-}) => <MeetingsView clubList={clubListData} />;
-
-MeetingsPage.getInitialProps = async () => {
-  const clubListData: IClub[] = await getClubList();
-
-  return { clubListData };
-};
+const MeetingsPage: NextFunctionComponent = () => <MeetingsView />;
 
 export default MeetingsPage;
